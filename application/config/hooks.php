429bc1136f06fd4796be953f8d3972f94099ebe6
Eric Tsang <surplus.et@gmail.com>
2015-01-20 21:47:34 -0800

    got basic hook working
    * got a basic hook to work
    
    todo: must make the hook do some fancy parsing of some sort in a clean and
    sexy way
    
    i'm going to sleep now
    
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -10,7 +10,12 @@
 |
 */
 
-
+$hook['display_override'] = array(
+        'class'    => 'Masseur',
+        'function' => 'massage_output',
+        'filename' => 'Masseur.php',
+        'filepath' => 'hooks'
+);
 
 /* End of file hooks.php */
 /* Location: ./application/config/hooks.php */
--- application/hooks/Masseur.php
+++ application/hooks/Masseur.php
@@ -3,6 +3,13 @@
 class Masseur
 {
 
+    function massage_output()
+    {
+        $CI = &get_instance();
+        echo $CI->get_instance()->output->get_output();
+        echo 'eric';
+    }
+
 }
 
 /* End of file Masseur.php */
